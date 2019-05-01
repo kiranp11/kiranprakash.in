c371dfd22173960b3e5f7153654205cef25eee48
Kiran Prakash <kprakash@thoughtworks.com>
2019-05-01 12:13:29 +0200

    added talks section

--- config.js
+++ config.js
@@ -16,6 +16,10 @@ module.exports = {
     {
       label: 'About me',
       path: '/pages/about'
+    },
+    {
+      label: 'Talks',
+      path: '/pages/talks'
     }
   ],
   author: {
---
+++ content/pages/talks.md
@@ -0,0 +1,6 @@
+---
+title: "Talks"
+template: "page"
+---
+
++ [hub.belin - 11'th April 2019](https://www.hub.berlin/programme-2019/curse-data-lake-monster) 
